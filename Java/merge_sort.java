95fb7ac77551f9ad3f86272982495fa70decbced
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2023-10-04 01:41:08 +0530

    Adding Java Programs

--- Java/merge_sort.java
+++ Java/merge_sort.java
@@ -1,49 +1,74 @@
-public class merge_sort {
-    public static void conquer(int arr[],int si,int mid,int ei) {
-        int merge[]=new int[ei-si+1];
-        int idx1=si;
-        int idx2=mid+1;
-        int x=0;
-
-        while(idx1<=mid&&idx2<=ei)
-        {
-            if(arr[idx1]<=arr[idx2]){
-                merge[x]=arr[idx1];
-                x++;idx1++;
-            }else{
-                merge[x++]=arr[idx2++];
+/* Java program for Merge Sort */
+class MergeSort 
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+  
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+  
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+  
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
             }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
         }
-        while(idx1<=mid){
-            merge[x++]=arr[idx1++];
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
         }
-        while(idx2<=ei){
-            merge[x++]=arr[idx2++];
-        }
-        for(int i=0,j=si;i<merge.length;i++,j++)
-        {
-            arr[j]=merge[i];
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
         }
     }
-    public static void divide(int arr[],int si,int ei) {
-        if(si>=ei)
-        {
-            return;
+  
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+  
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+  
+            // Merge the sorted halves
+            merge(arr, l, m, r);
         }
-        int mid=si+(ei-si)/2;//(si+ei)/2 problem here is this if si and ei is both large 
-                             //then they can exceed the integer which results in problem in space complexity
-        divide(arr, si, mid);
-        divide(arr, mid+1, ei);
-        conquer(arr,si,mid,ei);                 
     }
-    public static void main(String[] args) {
-        int arr[]={6,3,9,5,2,8};
-        int n=arr.length;
-        divide(arr, 0, n-1);
-        //print
-        for(int i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }System.out.println();
-    }
+  
 }
